8bf465b711131188b7bfb5e20aab8376037c8f7f
LFY <hallelujah@arch.home>
2018-03-01 20:29:14 +0100

    Scalp algorithm running on (G)BM, 3nd commit : basic methods, poor results, no fee on actions, no adaptive algorithm to avoid risk.

--- np.py
+++ np.py
@@ -6,27 +6,28 @@ from math import floor
 from matplotlib.finance import candlestick_ohlc
 import datetime
 
-N = 400
-USDBTC = np.zeros(shape=(N,4))
-delta = 8
-T = 1600
+#Parameters brownian()
+#
+#x[0]:      X(0), initial stock price
+#N:         number of increments
+#T:         time period
+#dt:        time step
+#delta:     "speed" of the Brownian motion, variance = delta**2t
+N = 500
+T = 400
 dt = T/N
+delta = .1
+delta2 = .05
 x = np.empty((N+1))
 xL = np.empty((N+1))
 xH = np.empty((N+1))
 dxL = np.empty((N+1))
 dxH = np.empty((N+1))
-t = np.arange(0, T, dt)
-x[0] = 1000
+x[0] = 10
 xL[0] = 0.0
 xH[0] = xL[0]
-
-volatility = 16
-stochrsit = 5
-USDBTC[stochrsit,1] = 0
-for i in range(0, stochrsit+1) :
-    USDBTC[i,0] = 100
-srsibin = np.empty((N-stochrsit))
+t = np.arange(0, T, dt)
+portfolio = np.zeros(shape=(N,2))
 
 def brownian(x0, n, dt, delta, out=None):
     x0 = np.asarray(x0)
@@ -37,15 +38,65 @@ def brownian(x0, n, dt, delta, out=None):
     out += np.expand_dims(x0, axis=-1)
     return out
 
+def brownianu(s, N):
+    np.random.seed(s)
+    return np.cumsum(np.random.normal(0., 1., N)*sqrt(1./N))
+
+#Parameters GBM()
+#
+#x[0]:      X(0), initial stock price (used by brownian())
+#mu:        drift coefficient
+#sigma:     volatility coefficient
+#x:         brownian motion calculated by brownian()
+#t:         np array, 0, dt, 2dt, ..., T
+
+mu = 0.05
+sigma = 0.04
+
+def GBM(x0, mu, sigma, x, T, N):
+    t = np.linspace(0., 1., N+1)
+    S = []
+    S.append(x0)
+    for i in range(1, N+1):
+        dr = (mu - 0.5 * sigma**2)*t[i]
+        diff = sigma * x[i-1]
+        S.append(x0*np.exp(dr+diff))
+    return S, t
+
+#Parameters stochrsi()
+#
+#x:         numpy array, prices (brownian, GBM, real datas)
+#i:         stochrsi() computes the stochrsi of x at i
+#stochrsit: stochrsi based on the last stochrsit prices
+
+stochrsit = 15
+portfolio[stochrsit,1] = 0 #initial condition (qty of money 1)
+for i in range(0, stochrsit+1) :
+    portfolio[i,0] = 100 #initial condition (qty of money 0)
+srsifiltered = np.empty((N-stochrsit))
+
 def stochrsi(x, i, stochrsit):
     xH = np.amax(x[i-stochrsit:i])
     xL = np.amin(x[i-stochrsit:i])
     return (x[i-1]-xL)/(xH-xL)
 
-def achat(x, t, q):
+#Paramters stochrsib()
+#
+#srsi:  numpy array of the values calculated by stochrsi()
+def stochrsib(srsi):
+    for i in range(0, len(srsi)):
+        if(srsi[i]<0.1): #treshold
+            srsifiltered[i] = 0
+        elif(srsi[i]>0.7): #treshold
+            srsifiltered[i] = 1
+        else:
+            srsifiltered[i]=srsi[i]
+    return srsifiltered
+
+def buy(x, t, q):
     return [-q],[q/x[t]]
 
-def vente(x, t, q):
+def sell(x, t, q):
     return [q*x[t]],[-q]
 
 def plotsrsi(t, srsi, stochrsit, name):
@@ -54,88 +105,77 @@ def plotsrsi(t, srsi, stochrsit, name):
     plt.plot(np.arange(stochrsit*dt, T, dt), srsi)
     plt.savefig(name+'.png')
 
-def plotusdbtc(i, usdbtc, lsrsi, name, x):
+def plotportfolio(i, portfolio, lsrsi, name, x):
     fig, ax = plt.subplots()
-    tplub = np.arange(stochrsit, lsrsi+stochrsit-1)
+    tplub = np.arange(stochrsit, lsrsi+stochrsit)
     if i==1:
-        p = np.delete([tuple([usdbtc[i, 0]+x[i]*usdbtc[i, 1]]) for i in range(len(tplub)+1)], 1, axis=0)
+        p = np.delete([tuple([portfolio[i, 0]+x[i]*portfolio[i, 1]]) for i in range((len(tplub)+1))], 1, axis=0)
         plt.plot(tplub, p)
-        """plt.plot(np.arange(stochrsit, stochrsit+len(np.delete(usdbtc,np.s_[0:3], axis=1))*dt,dt), np.delete(usdbtc,np.s_[0:3], axis=1))"""
     else:
-        p = np.delete([tuple([usdbtc[i, 0]/x[i]+usdbtc[i, 1]]) for i in range(len(tplub)+1)], 1, axis=0)
+        p = np.delete([tuple([portfolio[i, 0]/x[i]+portfolio[i, 1]]) for i in range((len(tplub)+1))], 1, axis=0)
         plt.plot(tplub, p)
-        """plt.plot(np.arange(stochrsit, stochrsit+len(np.delete(usdbtc,np.s_[0:3], axis=1))*dt,dt), np.delete(np.delete(usdbtc,np.s_[0:2], axis=1), np.s_[3:4], axis=1))"""
     plt.savefig(name+'.png')
 
-def plotsrsibin(t, srsibin, stochrsit, name):
+def plotsrsifiltered(t, srsifiltered, stochrsit, name):
     fig, ax = plt.subplots()
-    plt.plot(np.arange(stochrsit*dt, T, dt), srsibin)
+    plt.plot(np.arange(stochrsit*dt, T, dt), srsifiltered)
     plt.savefig(name+'.png')
 
-def plotbrownian(x, dxH, dxL, dt, t, name):
+def plotbrownian(x, dxH, dxL, dt, t, name, b):
     dxL = -abs(dxL)
     dxH = abs(dxH)
-    for i in range(len(t)+1):
+    for i in range(len(x)):
         dxL[i] *= (x[i]-x[i-1])/x[i]
         dxH[i] *= (x[i]-x[i-1])/x[i]
     quotes = [tuple([t[i],
                      x[i],
                      x[i+1]+dxH[i+1],
                      x[i+1]+dxL[i+1],
-                     x[i+1]]) for i in range(len(t))]
+                     x[i+1]]) for i in range(b,len(x)-1)]
     fig, ax = plt.subplots()
     candlestick_ohlc(ax, quotes, width=dt, colorup='g', colordown='r', alpha=1.0)
     plt.savefig(name + '.png')
-
-def stochrsib(srsi):
-    for i in range(0, len(srsi)):
-        if(srsi[i]<0.5):
-            srsibin[i] = 0
-        elif(srsi[i]>0.5):
-            srsibin[i] = 1
-        else:
-            srsibin[i]=srsi[i]
-    return srsibin
 
 def stochrsiarray(x, t, stochrsit):
     return np.delete([tuple([stochrsi(x, i, stochrsit)]) for i in range(stochrsit,len(t)+1)], 1, axis=0)
 
 """le choix de treshold dépendra des risques de l'action : risque qu'on souhaite encourir (à voir si je définis ça par rapport à un gain min ou pas seulement) (si élevé alors en vente : treshold très important, on hold plus, en vente : treshold élevé, on hold plus, &inversement), à réfléchir si on all-in à chaque fois ou si l'on met en jeu f(srsi)*capital"""
 
-def ag1(stochrsit, t, x, USDBTC, srsib, srsi, dt, p):
+def method1(stochrsit, t, x, portfolio, srsib, srsi, dt, p):
     for i in range(stochrsit, len(srsi)+stochrsit-1):
-        if USDBTC[i, 0] != 0 and srsib[i-stochrsit] == 0 :
-            USDBTC[i+1,0] = USDBTC[i, 0] + achat(x, i+1, (1-srsi[i-stochrsit])**p*USDBTC[i,0])[0]
-            USDBTC[i+1,1] = USDBTC[i, 1] + achat(x, i+1, (1-srsi[i-stochrsit])**p*USDBTC[i,0])[1]
+        if portfolio[i, 0] != 0 and srsib[i-stochrsit] == 0 :
+            portfolio[i+1,0] = portfolio[i, 0] + buy(x, i+1, (1-srsi[i-stochrsit])**p*portfolio[i,0])[0]
+            portfolio[i+1,1] = portfolio[i, 1] + buy(x, i+1, (1-srsi[i-stochrsit])**p*portfolio[i,0])[1]
             with open("run.txt", "a") as myfile:
-                myfile.write("USDBTC[][]B : " + str(USDBTC[i,0]) + "," + str(USDBTC[i,1]) + " -> " + str(USDBTC[i+1,0])+","+str(USDBTC[i+1,1]) + "*" + str(x[i+1]) + "\n")
-        elif USDBTC[i, 1] != 0 and srsib[i-stochrsit] == 1:
-            USDBTC[i+1,0] = USDBTC[i,0] + vente(x, i+1, (srsi[i-stochrsit]*USDBTC[i, 1])**p)[0]
-            USDBTC[i+1,1] = USDBTC[i,1] + vente(x, i+1, (srsi[i-stochrsit]*USDBTC[i, 1])**p)[1]
+                myfile.write("portfolio[][]B : " + str(portfolio[i,0]) + "," + str(portfolio[i,1]) + " -> " + str(portfolio[i+1,0])+","+str(portfolio[i+1,1]) + "*" + str(x[i+1]) + "\n")
+        elif portfolio[i, 1] != 0 and srsib[i-stochrsit] == 1:
+            portfolio[i+1,0] = portfolio[i,0] + sell(x, i+1, (srsi[i-stochrsit]*portfolio[i, 1])**p)[0]
+            portfolio[i+1,1] = portfolio[i,1] + sell(x, i+1, (srsi[i-stochrsit]*portfolio[i, 1])**p)[1]
             with open("run.txt", "a") as myfile:
-                myfile.write("USDBTC[][]S : " + str(USDBTC[i, 0]) + "," + str(USDBTC[i, 1]) + " -> " + str(USDBTC[i+1,0]) + "," + str(USDBTC[i+1,1]) + "*" + str(x[i+1])+ "\n")
+                myfile.write("portfolio[][]S : " + str(portfolio[i, 0]) + "," + str(portfolio[i, 1]) + " -> " + str(portfolio[i+1,0]) + "," + str(portfolio[i+1,1]) + "*" + str(x[i+1])+ "\n")
         else :
-            USDBTC[i+1, 0] = USDBTC[i, 0]
-            USDBTC[i+1, 1] = USDBTC[i, 1]
+            portfolio[i+1, 0] = portfolio[i, 0]
+            portfolio[i+1, 1] = portfolio[i, 1]
             with open("run.txt", "a") as myfile:
-                myfile.write("USDBTC[][]K : " + str(USDBTC[i, 0]) + "," + str(USDBTC[i, 1])+ " -> " + str(USDBTC[i+1,0]) + "," + str(USDBTC[i+1,1])+"*"+str(x[i+1])+"\n")
-        """USDBTC[i, 2] = USDBTC[i, 0]+x[i]*USDBTC[i, 1]"""
-        """USDBTC[i, 3] = USDBTC[i, 0]/x[i]+USDBTC[i, 1]"""
-    return USDBTC
+                myfile.write("portfolio[][]K : " + str(portfolio[i, 0]) + "," + str(portfolio[i, 1])+ " -> " + str(portfolio[i+1,0]) + "," + str(portfolio[i+1,1])+"*"+str(x[i+1])+"\n")
+    return portfolio
 
-
-def run(x, N, dt, delta, dxL, volatility, dxH, t, i, USDBTC, p):
+def run(x, N, dt, delta, dxL, delta2, dxH, t, i, portfolio, p):
     brownian(x[0], N, dt, delta, out=x[1:])
-    brownian(dxL[0], N, dt, volatility, out=dxL[1:])
-    brownian(dxH[0], N, dt, volatility, out=dxH[1:])
-    plotbrownian(x, dxH*0, dxL*0, dt, t, 'plotb'+str(i))
+    '''brownian(dxL[0], N, dt, delta2, out=dxL[1:])
+    brownian(dxH[0], N, dt, delta2, out=dxH[1:])'''
+    plotbrownian(x, dxH*0, dxL*0, dt, t, 'plotb'+str(i), 0)
+    xgbm = GBM(x[0], mu, sigma, x, T, N)[0]
+    plotbrownian(xgbm, dxH*0, dxL*0, dt, t, 'plotgbm'+str(i), 1)
     srsi = stochrsiarray(x, t, stochrsit)
     srsib = stochrsib(srsi)
-    plotsrsibin(t, srsib, stochrsit, 'plotsrsibin'+str(i))
+    plotsrsifiltered(t, srsib, stochrsit, 'plotsrsifiltered'+str(i))
     plotsrsi(t, srsi, stochrsit, 'plotsrsi'+str(i))
-    USDBTC = ag1(stochrsit, t, x, USDBTC, srsib, srsi, dt, p)
-    plotusdbtc(1, USDBTC, len(srsi), 'plotusdbtc1'+str(i), x)
-    plotusdbtc(0, USDBTC, len(srsi), 'plotusdbtc0'+str(i), x)
-
+    portfolio = method1(stochrsit, t, x, portfolio, srsib, srsi, dt, p)
+    plotportfolio(1, portfolio, len(srsi), 'plotportfolio1b'+str(i), x)
+    plotportfolio(0, portfolio, len(srsi), 'plotportfolio0b'+str(i), x)
+    portfolio2 = method1(stochrsit, t, xgbm, portfolio, srsib, srsi, dt, p)
+    plotportfolio(1, portfolio2, len(srsi), 'plotportfolio1gb'+str(i), x)
+    plotportfolio(0, portfolio2, len(srsi), 'plotportfolio0gb'+str(i), x)
 for i in range(0, 1):
-    run(x, N, dt, delta, dxL, volatility, dxH, t, i, USDBTC, 1)
+    run(x, N, dt, delta, dxL, delta2, dxH, t, i, portfolio, 1)
